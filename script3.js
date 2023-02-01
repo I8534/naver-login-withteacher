833de528a96db0e8debe0017c85b2e97af5acafb
I8534 <I85341266@GMAIL.COM>
2023-02-01 16:07:28 +0900

    🔥 최종

--- script3.js
+++ script3.js
@@ -346,7 +346,74 @@ $("#veritext").focusout(function(){
     $("#phone .warn").html('<span class="text-red">인증번호를 다시 확인해주세요.</span>');
     $(this).parent('.inputbox').addClass('border-red');
   }
+})
 
+// 주소
+// 카카오에서 제공하는 주소찾기 api 활용
+// API란? Application Programming Interface의 줄임말
+// 프론트앤드(클라이언트) 와 백앤드(서버)가 요청과 응답을 받을 수 있게 만들어진 체계
+// api 요청에서 가장 중요한 개념은 CRUD이다. 
+// CREAT, Read, Update, Delete 이 네가지 api 요청의 기본 요소 네가지이다.
+// API를 만드는 역할을 하는 것이 서버 개발지이다. = 즉 백엔드 개발자
 
+function sample6_execDaumPostcode() {
+  addressveri = true;
+  new daum.Postcode({
+      oncomplete: function(data) {
+          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
+          // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+          // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+          var addr = ''; // 주소 변수
+          var extraAddr = ''; // 참고항목 변수
+
+          //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+          if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+              addr = data.roadAddress;
+          } else { // 사용자가 지번 주소를 선택했을 경우(J)
+              addr = data.jibunAddress;
+          }
+
+          // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+          if(data.userSelectedType === 'R'){
+              // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+              // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+              if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                  extraAddr += data.bname;
+              }
+              // 건물명이 있고, 공동주택일 경우 추가한다.
+              if(data.buildingName !== '' && data.apartment === 'Y'){
+                  extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+              }
+              // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+              if(extraAddr !== ''){
+                  extraAddr = ' (' + extraAddr + ')';
+              }
+              // 조합된 참고항목을 해당 필드에 넣는다.
+              document.getElementById("sample6_extraAddress").value = extraAddr;
+          
+          } else {
+              document.getElementById("sample6_extraAddress").value = '';
+          }
+
+          // 우편번호와 주소 정보를 해당 필드에 넣는다.
+          document.getElementById('sample6_postcode').value = data.zonecode;
+          document.getElementById("sample6_address").value = addr;
+          // 커서를 상세주소 필드로 이동한다.
+          document.getElementById("sample6_detailAddress").focus();
+      }
+  }).open();
+}
+
+// #joinbtn을 눌렀을 때 필수 요소가 모두 true라면(조건!)
+// #join-form을 submit (실행문)
+
+$("#joinbtn").click(function() {
+
+  if (idveri && pwveri && pwchkveri && nameveri && birthveri && genderveri && phoneveri && addressveri) {
+    $("#joinbtn").attr("type", "submit");
+  } else {
+    // input을 강제로 포커스아웃 시키는 이벤트를 일으킨다.
+    $("input").trigger("focusout");
+  }
 })
